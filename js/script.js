57bced571617a5b11967c0980754d65f59f2c40a
Dominik Wilusz <wilusz1@o2.pl>
2018-08-24 01:30:12 +0200

    Add style

--- js/script.js
+++ js/script.js
@@ -25,15 +25,14 @@ $(function() {
         var $area;
 
         $countriesList.empty();
-        console.log(resp);
 
         resp.forEach(function(item) {
-            $li = $('<li>').addClass('countryCard');
-            $region = $('<p>').addClass('countryName').text(item.region).appendTo($li);
-            $name = $('<p>').addClass('countryCapital').text(item.name).appendTo($li);
-            $capital = $('<p>').addClass('countryRegion').text(item.capital).appendTo($li);
-            $population = $('<p>').addClass('countryPopulation').text(item.population).appendTo($li);
-            $area = $('<p>').addClass('countryArea').text(item.area + 'm^2').appendTo($li);
+            $li = $('<li>').addClass('country-card');
+            $region = $('<p>').addClass('country-region').text(item.region).appendTo($li);
+            $name = $('<p>').addClass('country-capital').text('Country: ' + item.name).appendTo($li);
+            $capital = $('<p>').addClass('country-name').text('Capital: ' + item.capital).appendTo($li);
+            $population = $('<p>').addClass('country-population').text('Populatioin: ' + item.population).appendTo($li);
+            $area = $('<p>').addClass('country-area').text('Area: ' + item.area + ' m^2').appendTo($li);
             $li.appendTo($countriesList);
         });
     };
