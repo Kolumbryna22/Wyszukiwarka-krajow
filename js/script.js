53f12cb26776e92a79e3a49e35aed586674540bd
Dominik Wilusz <wilusz1@o2.pl>
2018-08-23 22:28:01 +0200

    Add more results for search

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ $(function() {
     var url = 'https://restcountries.eu/rest/v1/name/';
     var $countriesList = $('#countries');
 
-    function searchCountries() {
+    function searchCountries(event) {
         var $countryName = $('#countryName').val();
+
+        event.preventDefault();
 
         $.ajax({
             url: url + $countryName,
@@ -15,10 +17,23 @@ $(function() {
     };
 
     function showCountriesList(resp) {
+        var $div = $('<div>').addClass('countryCard');
+        var $name = $('<p>').addClass('countryName');
+        var $capital = $('<p>').addClass('countryCapital');
+        var $region = $('<p>').addClass('countryRegion');
+        var $population = $('<p>').addClass('countryPopulation');
+        var $area = $('<p>').addClass('countryArea');
+
         $countriesList.empty();
 
         resp.forEach(function(item) {
-            $('<li>').text(item.name).appendTo($countriesList);
+            $region.text(item.region).appendTo($div);
+            $name.text(item.name).appendTo($div);
+            $capital.text(item.capital).appendTo($div);
+            $population.text(item.population).appendTo($div);
+            $area.text(item.area + ' m^2').appendTo($div);
+            $div.appendTo($countriesList);
+
         });
     };
 
